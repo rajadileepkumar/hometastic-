6934042c73e5ca4fd6e7125efb743688bbd676c0
rajadileepkuma34@gmail.com <rajadileepkumar34@gmail.com>
2016-05-09 18:29:36 +0530

    version 1.1.3

---
+++ inc/contact.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+	Template Name:Contact
+*/
+get_header();?>
+<section>
+	<!-- Google Map -->
+    <div class="google-map-container">
+        <div class="google-map">
+            
+        </div>
+    </div>
+    <!-- /Google Map -->
+		<div class="margin-10"></div>
+</section>
+
+<?php get_footer();?>
